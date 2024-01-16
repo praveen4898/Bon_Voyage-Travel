2fcc7fccc7d0b025857d2b990f3ef82b0e2daed0
praveen <praveenravi4898@gmail.com>
2024-01-16 14:36:27 +0530

    structure created

---
+++ src/Redux/actiontype.js
@@ -0,0 +1,25 @@
+
+
+
+///tofetch all the packages & if filter added based on it it will dispatch
+export const GET_DESTINATION_REQUEST = "GET_DATA_REQUEST";
+export const GET_DESTINATION_SUCCESS = "GET_DATA_SUCCESS";
+export const GET_DESTINATION_FAILURE = "GET_DATA_FAILURE";
+
+
+///when clicked on book button on a paclage,this actions are dispatched
+export const GET_BOOKINGS_DATA_REQUEST = "GET_BOOKINGS_DATA_REQUEST";
+export const GET_BOOKINGS_DATA_SUCCESS = "GET_BOOKINGS_DATA_SUCCESS";
+export const GET_BOOKINGS_DATA_FAILURE = "GET_BOOKINGS_DATA_FAILURE";
+
+
+//from admin, they can addnew packages to the existing choices
+export const ADD_DATA_REQUEST = "ADD_DATA_REQUEST";
+export const ADD_DATA_SUCCESS = "ADD_DATA_SUCCESS";
+export const ADD_DATA_FAILURE = "ADD_DATA_FAILURE";
+
+
+//in the booking page if they want to delete the booking of a package
+export const DELETE_DATA_REQUEST = "DELETE_DATA_REQUEST";
+export const DELETE_DATA_SUCCESS = "DELETE_DATA_SUCCESS";
+export const DELETE_DATA_FAILURE = "DELETE_DATA_FAILURE";
---
+++ src/Redux/reducer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+let initialstate={
+    isloading:false,
+    iserror:false,
+    destination:[],
+    mybooking:[],
+}
+
+
+const Reducer = () => {
+
+
+
+  return (
+    <div>Reducer</div>
+  )
+}
+
+export default Reducer
