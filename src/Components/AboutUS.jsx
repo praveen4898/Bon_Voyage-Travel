49fb4b3bbef332300893a5ee2152e5bcf036f618
Nidhi <nidhis2k02@gmail.com>
2024-01-22 02:16:59 +0530

    changesInLayout

--- src/Components/AboutUS.jsx
+++ src/Components/AboutUS.jsx
@@ -1,7 +1,5 @@
-
-
+import {Text} from '@chakra-ui/react'
 import React from 'react';
-
 const AboutUs = () => {
   return (
     <div>
@@ -11,7 +9,7 @@ const AboutUs = () => {
           marginTop: '-50px',
           width: '110%',
           height: '120vh',
-          backgroundImage:'url("https://media.istockphoto.com/id/526845820/photo/tokyo-tower-night-view.jpg?s=612x612&w=0&k=20&c=mm3hbBKG7i72YYQE-UsRqxMNJNrfTQfn4Ngf06fCNTw=")',
+          // backgroundImage:'url("https://media.istockphoto.com/id/526845820/photo/tokyo-tower-night-view.jpg?s=612x612&w=0&k=20&c=mm3hbBKG7i72YYQE-UsRqxMNJNrfTQfn4Ngf06fCNTw=")',
           backgroundPosition: 'center',
           backgroundSize: 'cover',
           filter: 'blur(10px)',
@@ -23,22 +21,22 @@ const AboutUs = () => {
 
       <section className="main_heading my-5">
         <div className="text-center">
-          <h1 className="display-4" style={{ color: 'white' , fontSize:"60px"}}>
-            ABOUT US
-          </h1>
+        <Text fontWeight="bold" mb={4} color="teal.500" fontFamily="cursive" fontSize="60px" p={"20px"}> 
+       About Us   
+        </Text>
           <hr className="w-25 mx-auto" />
         </div>
       </section>
-      <div style={{display:"flex" , textAlign: 'justify', gap:"50px", margin:"50px"}}>
-        <img src="https://code-symphony.netlify.app/public/front-view-beautiful-young-businesswoman-black-jacket-blue-shirt-observing-little-globe-smiling-front-table-business-job-office.jpg" alt="..." style={{width:"600px" , height:"300px"}}/>
+      <div style={{display:"flex" , textAlign: 'justify', gap:"50px", margin:"50px" }} backgroundImage={'linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url("https://img.freepik.com/free-vector/watercolor-mint-background_23-2150241023.jpg")'} >
+        <img src="https://code-symphony.netlify.app/public/front-view-beautiful-young-businesswoman-black-jacket-blue-shirt-observing-little-globe-smiling-front-table-business-job-office.jpg" alt="..." style={{width:"600px" , height:"300px" , borderRadius:"20px"}}/>
         <div>
-            <h1 style={{color:"white", fontSize:"40px"}}>Our Goal</h1>
-            <p style={{color:"white"}}>
+            <h1 style={{color:"black",  fontSize:"50px", fontWeight:"bolder"}}>Our Goal</h1>
+            <p style={{color:"black"}}>
                 <br />
-                Welcome to Travel code symphony, where we turn your travel dreams
+                Welcome to Bon Voyage, where we turn your travel dreams
               into reality. Our passion for exploration and adventure has
               inspired us to create a platform that caters to the wanderlust in
-              you. At Travel code symphony, we believe that every journey is a
+              you. At Bon Voyage, we believe that every journey is a
               story waiting to be told. Whether you're an avid traveler seeking
               new horizons or someone planning their first escape, we are here
               to make your travel experiences seamless and unforgettable.
@@ -51,26 +49,26 @@ const AboutUs = () => {
 
       <div style={{display:"flex" , textAlign: 'justify', gap:"50px", margin:"50px"}}>
       <div>
-            <h1 style={{color:"white", fontSize:"40px"}}>Our Journey</h1>
-            <p style={{color:"white"}}>
+            <h1 style={{color:"black", fontSize:"50px", fontWeight:"bolder"}}>Our Journey</h1>
+            <p style={{color:"black"}}>
                 <br />
                 Our team is comprised of seasoned travelers, travel enthusiasts,
               and experts in the field who are dedicated to curating exceptional
               travel experiences. From exotic destinations to
               off-the-beaten-path gems, we strive to provide you with a diverse
               array of options. Discover the world with confidence, knowing that
-              Travel code symphony is with you every step of the way. Join us on
+              Bon Voyage is with you every step of the way. Join us on
               this exciting journey, and let's create memories that will last a
               lifetime.
 
             </p>
         </div>
-        <img src="https://code-symphony.netlify.app/public/i-am-here-help-you-with-your-booking-travel-questions-female-sales-agent-holding-plane-tickets-while-smiling-making-eye-contact-travel-agency.jpg" alt="..." style={{width:"600px" , height:"300px"}}/>
+        <img src="https://code-symphony.netlify.app/public/i-am-here-help-you-with-your-booking-travel-questions-female-sales-agent-holding-plane-tickets-while-smiling-making-eye-contact-travel-agency.jpg" alt="..." style={{width:"600px" , height:"300px", borderRadius:"20px"}}/>
       </div>
 
       {/* Frequently Asked Questions Section */}
-      <section className="faq" style={{ margin: '5%', color: 'white' }}>
-        <h2 style={{ textAlign: 'center' }}>Frequently Asked Questions (FAQs)</h2>
+      <section className="faq" style={{ margin: '5%', color: 'black' }}>
+        <h2 style={{ textAlign: 'center', fontSize:"50px", fontFamily:"bolder" }}>Frequently Asked Questions (FAQs)</h2>
         <hr />
         <ul>
           <p>
