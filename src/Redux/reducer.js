ad5226a0e4abd9243b9c0d3ca194ea51d2195500
Deepak Underiya <underiyadeepak@gmail.com>
2024-01-22 01:36:36 +0530

    check

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import { GET_BOOKINGS_DATA_FAILURE, GET_BOOKINGS_DATA_REQUEST, GET_BOOKINGS_DATA_SUCCESS, 
-  GET_DESTINATION_FAILURE, GET_DESTINATION_REQUEST, GET_DESTINATION_SUCCESS,DELETE_BOOKINGS_DATA_REQUEST,DELETE_BOOKINGS_DATA_FAILURE,DELETE_BOOKINGS_DATA_SUCCESS } from './actiontype';
+import React from "react";
+import {
+  GET_BOOKINGS_DATA_FAILURE,
+  GET_BOOKINGS_DATA_REQUEST,
+  GET_BOOKINGS_DATA_SUCCESS,
+  GET_DESTINATION_FAILURE,
+  GET_DESTINATION_REQUEST,
+  GET_DESTINATION_SUCCESS,
+  DELETE_BOOKINGS_DATA_REQUEST,
+  DELETE_BOOKINGS_DATA_FAILURE,
+  DELETE_BOOKINGS_DATA_SUCCESS,
+} from "./actiontype";
 
-let initialstate={
-    isloading:false,
-    iserror:false,
-    destination:[],
-    mybooking:[],
-}
-
+let initialstate = {
+  isloading: false,
+  iserror: false,
+  destination: [],
+  mybooking: [],
+};
 
 const reducer = (state = initialstate, action) => {
   switch (action.type) {
@@ -20,28 +28,32 @@ const reducer = (state = initialstate, action) => {
     case GET_DESTINATION_FAILURE:
       return { ...state, isloading: false, iserror: true };
 
-  
-/////satmrngpraveenchanges
-      case GET_BOOKINGS_DATA_REQUEST:
-        return {...state,isloading:true}
-      
-      case GET_BOOKINGS_DATA_SUCCESS:
-        return{...state,isloading:false,mybooking:action.payload}
-      
-        case GET_BOOKINGS_DATA_FAILURE:
-          return{...state,isloading:false,iserror:true}
-      
-        
-          case DELETE_BOOKINGS_DATA_REQUEST:
-            return {...state,isloading:true}
-          
-          case DELETE_BOOKINGS_DATA_SUCCESS:
-            const updatedbooking=state.mybooking.filter((booking)=>booking.id!=action.payload)
-            return{...state,isloading:false,mybooking:updatedbooking,iserror:false}
-          
-            case DELETE_BOOKINGS_DATA_FAILURE:
-              return{...state,isloading:false,iserror:true}
+    /////satmrngpraveenchanges
+    case GET_BOOKINGS_DATA_REQUEST:
+      return { ...state, isloading: true };
 
+    case GET_BOOKINGS_DATA_SUCCESS:
+      return { ...state, isloading: false, mybooking: action.payload };
+
+    case GET_BOOKINGS_DATA_FAILURE:
+      return { ...state, isloading: false, iserror: true };
+
+    case DELETE_BOOKINGS_DATA_REQUEST:
+      return { ...state, isloading: true };
+
+    case DELETE_BOOKINGS_DATA_SUCCESS:
+      const updatedbooking = state.mybooking.filter(
+        (booking) => booking.id != action.payload
+      );
+      return {
+        ...state,
+        isloading: false,
+        mybooking: updatedbooking,
+        iserror: false,
+      };
+
+    case DELETE_BOOKINGS_DATA_FAILURE:
+      return { ...state, isloading: false, iserror: true };
 
     default:
       return state;
